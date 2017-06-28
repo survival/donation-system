670c915bebe7d6407ad91f39c439a87f91a74211
Sam Stickland <sam@hubbado.com>
2017-06-28 15:36:49 +0100

    Skeleton implementation of Payment.process

--- lib/payment.rb
+++ lib/payment.rb
@@ -1,9 +1,15 @@
 # frozen_string_literal: true
 
 class Payment
+  Response = Struct.new(:request)
+
   attr_accessor :request
 
   def initialize(request)
     @request = request
+  end
+
+  def process
+    Response.new(request)
   end
 end
--- spec/payment_spec.rb
+++ spec/payment_spec.rb
@@ -8,6 +8,17 @@ describe Payment do
   it 'stores the request object passed in the initializer' do
     request = Request.new('bob')
     payment = Payment.new(request)
+
     expect(payment.request).to eq request
+  end
+
+  describe '#process' do
+    it 'returns a response object' do
+      request = Request.new('bob')
+      payment = Payment.new(request)
+
+      response = payment.process
+      expect(response.request).to eq request
+    end
   end
 end
