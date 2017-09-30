d52767b50a54abcb167ebff68e24efa008f4ae29
octopusinvitro <octopusinvitro@users.noreply.github.com>
2017-09-30 20:20:27 +0100

    First stab at the supporter database logic
    This class tries to represent the business logic of adding a donation.
    Basically:
    
    * search supporter by email
    * if it doesn't exist, create it
    * create a donation using the supporter
    
    We use command objects for each action, and collect their errors
    in each step. If there are errors, we return the control to the
    caller, sending the errors.

--- lib/salesforce/database.rb
+++ lib/salesforce/database.rb
@@ -2,6 +2,67 @@
 
 module Salesforce
   class Database
-    def self.add_donation(_data); end
+    def self.add_donation(data)
+      new(data).add_donation
+    end
+
+    def initialize(data)
+      @data = data
+    end
+
+    def add_donation
+      return [:missing_email] unless email_present?
+      return supporter_result.errors unless supporter_result.okay?
+      create_donation.errors
+    end
+
+    class SupporterFinder
+      def self.execute(_field, _value); end
+    end
+
+    class SupporterCreator
+      def self.execute(_data); end
+    end
+
+    class DonationCreator
+      def self.execute(_data, _supporter); end
+    end
+
+    private
+
+    attr_reader :data
+
+    def email_present?
+      data.respond_to?(:email) && data.email
+    end
+
+    def supporter_result
+      @supporter_result ||= ensure_supporter_result
+    end
+
+    def ensure_supporter_result
+      return supporter_search_result if search_errors_or_supporter_exists?
+      create_supporter
+    end
+
+    def search_errors_or_supporter_exists?
+      supporter_search_result.errors? || supporter_search_result.item
+    end
+
+    def supporter
+      supporter_result.item
+    end
+
+    def supporter_search_result
+      @supporter_search_result ||= SupporterFinder.execute(:Email, data.email)
+    end
+
+    def create_supporter
+      SupporterCreator.execute(data)
+    end
+
+    def create_donation
+      DonationCreator.execute(data, supporter)
+    end
   end
 end
