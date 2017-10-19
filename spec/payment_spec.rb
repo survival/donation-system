b892a059fd70ad4f38f9fff1937eb5619a6a6397
octopusinvitro <octopusinvitro@users.noreply.github.com>
2017-10-19 17:35:30 +0100

    Plug the new stripe gateway in the payment class

--- spec/payment_spec.rb
+++ spec/payment_spec.rb
@@ -2,77 +2,53 @@
 
 require 'payment'
 require 'salesforce/database'
+require 'stripe/gateway'
 require 'spec_helper'
-require 'support/with_env'
 require 'thank_you_mailer'
 
 RSpec.describe Payment do
-  include Support::WithEnv
+  Request = Struct.new(:email, :name)
 
-  Request = Struct.new(
-    :amount, :currency, :card_number, :cvc, :exp_year, :exp_month, :email, :name
-  )
-
-  let(:request) { Request.new(nil) }
+  let(:request) { Request.new('user@example.com', 'Name') }
   let(:payment) { described_class.new(request) }
 
-  before { allow(Salesforce::Database).to receive(:add_donation) }
-
-  it 'stores the request object passed in the initializer' do
-    expect(payment.request).to eq(request)
+  describe 'when the gateway is unsuccessful' do
+    it 'returns the gateway error' do
+      gateway_result = Stripe::Result.new(nil, [:error])
+      allow(Stripe::Gateway).to receive(:charge).and_return(gateway_result)
+      expect(described_class.new(request).attempt).to eq([:error])
+    end
   end
 
-  describe '#attempt', vcr: { record: :once } do
-    it 'fails without an api key' do
-      with_env('STRIPE_API_KEY' => '') do
-        expect(payment.attempt).to eq([:invalid_request])
-      end
+  describe 'when the gateway is successful' do
+    let(:gateway_result) { Stripe::Result.new('irrelevant', []) }
+
+    before do
+      allow(Stripe::Gateway).to receive(:charge).and_return(gateway_result)
+      allow(Salesforce::Database).to receive(:add_donation).and_return([])
     end
 
-    it 'fails with an invalid api key' do
-      with_env('STRIPE_API_KEY' => 'aaaaa') do
-        expect(payment.attempt).to eq([:invalid_request])
-      end
+    it 'returns errors if there is a problem with the supporter database' do
+      errors = %i[foo bar]
+      allow(Salesforce::Database).to receive(:add_donation).and_return(errors)
+      expect(described_class.new(request).attempt).to eq(errors)
     end
 
-    it 'fails with a valid api key but no other parameters' do
-      expect(payment.attempt).to eq([:invalid_request])
+    it 'succeeds if there are no problems with the supporter database' do
+      expect(described_class.new(request).attempt).to be_empty
     end
 
-    it 'fails with a valid api key and invalid card number' do
-      request = Request.new(
-        '1000', 'usd', '1235424242424242', '123', '2020', '01',
-        'irrelevant', 'irrelevant'
-      )
-      payment = described_class.new(request)
-      expect(payment.attempt).to eq([:card_error])
+    it 'sends a thank you email' do
+      allow(ThankYouMailer).to receive(:send_email)
+      described_class.new(request).attempt
+      expect(ThankYouMailer).to have_received(:send_email)
+        .with('user@example.com', 'Name')
     end
 
-    context 'success' do
-      let(:request) do
-        Request.new(
-          '1000', 'usd', '4242424242424242', '123', '2020', '01',
-          'user@example.com', 'Name'
-        )
-      end
-
-      it 'succeeds with a valid api key and valid parameters' do
-        payment = described_class.new(request)
-        expect(payment.attempt).to be_empty
-      end
-
-      it 'should send a thank you email' do
-        allow(ThankYouMailer).to receive(:send_email)
-        described_class.new(request).attempt
-        expect(ThankYouMailer).to have_received(:send_email)
-          .with('user@example.com', 'Name')
-      end
-
-      it 'adds the donation to the supporters database' do
-        described_class.new(request).attempt
-        expect(Salesforce::Database).to have_received(:add_donation)
-          .with(request)
-      end
+    it 'adds the donation to the supporters database' do
+      described_class.new(request).attempt
+      expect(Salesforce::Database).to have_received(:add_donation)
+        .with(request)
     end
   end
 end
