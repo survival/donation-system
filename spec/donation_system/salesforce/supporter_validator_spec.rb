102f1fc0ec729bca19389d6dc02bc04d3fc47385
octopusinvitro <octopusinvitro@users.noreply.github.com>
2017-11-17 16:19:15 +0000

    Add the rest of fields to Salesforce supporters
    This adds the optional parameters (i.e., those that are not
    required in our Salesforce configuration) to the Supporter
    table.
    
    Some fields that are used in the old site were left out:
    
      * FirstName: we have to first separate both from the name
      field
    
      * Web_Login__c: we decided that we don't want to save
      supporter data in our database
    
      * OwnerId: it is defined in maloca as
      SalesforceUser.current_user.Id, but at this stage we don't
      have any way to deal with a Salesforce user.
    
      * Email_is_Web_Login_legacy__c: the new website won't have a
      login
    
    Also, at the moment LastName is still using the full name until
    we find a good way of separating it. I am using placeholders for
    the greeting for the same reason. Maloca's code is also
    truncating some fields, which for now we are not doing either.

--- spec/donation_system/salesforce/supporter_validator_spec.rb
+++ spec/donation_system/salesforce/supporter_validator_spec.rb
@@ -19,8 +19,53 @@ module DonationSystem
       end
 
       describe 'application required fields' do
-        it 'requires an email' do
+        it 'has required field email' do
           expect(fields[:Email]).to eq('user@example.com')
+        end
+
+        it 'has required field first entered' do
+          allow(Date).to receive(:today).and_return(Date.new(1000, 1, 1))
+          expect(fields[:First_entered__c].year).to eq(1000)
+        end
+      end
+
+      describe 'optional fields' do
+        it 'can have an address' do
+          expect(fields[:MailingStreet]).to eq('Address')
+        end
+
+        it 'can have a city' do
+          expect(fields[:MailingCity]).to eq('City')
+        end
+
+        it 'can have a state' do
+          expect(fields[:MailingState]).to eq('State')
+        end
+
+        it 'can have a ZIP code' do
+          expect(fields[:MailingPostalCode]).to eq('Z1PC0D3')
+        end
+
+        it 'can have a country' do
+          expect(fields[:MailingCountry]).to eq('Country')
+        end
+
+        it 'can have a greeting' do
+          expect(fields[:Greeting__c]).to eq('Hi')
+        end
+
+        it 'can have npe01 fields' do
+          expect(fields[:npe01__Private__c]).to be(false)
+          expect(fields[:npe01__SystemIsIndividual__c]).to be(true)
+        end
+
+        it 'can have preferences' do
+          expect(fields[:Do_not_email__c]).to be(false)
+        end
+
+        it 'can have personal data' do
+          expect(fields[:Couple__c]).to be(false)
+          expect(fields[:Deceased__c]).to be(false)
         end
       end
 
