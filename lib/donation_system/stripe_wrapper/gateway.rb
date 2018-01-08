ea4742ece4db5dd6a2a618badc0b662dd9be9b71
octopusinvitro <octopusinvitro@users.noreply.github.com>
2018-01-08 15:38:25 +0000

    Make the old Stripe gateway use the resource creator
    The old Stripe gateway class that we used for one-off donations
    was using a Stripe resource whose behavior has been encapsulated
    in the resource creator, so now we can replace that code with
    the creator.

--- lib/donation_system/stripe_wrapper/gateway.rb
+++ lib/donation_system/stripe_wrapper/gateway.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 require 'stripe'
-require_relative 'input_data_validator'
-require_relative 'result'
+
+require_relative 'fields_generator'
+require_relative 'resource_creator'
 
 module DonationSystem
   module StripeWrapper
@@ -13,61 +14,18 @@ module DonationSystem
 
       def initialize(data)
         @data = data
-        @api_errors = []
       end
 
       def charge
-        Result.new(charge_result, errors)
+        ResourceCreator.create(Stripe::Charge, fields_generator.for_charge)
       end
 
       private
 
-      attr_reader :data, :api_errors
+      attr_reader :data
 
-      def charge_result
-        charge_and_rescue_data_related_errors
-      rescue Stripe::RateLimitError
-        save_error(:too_many_requests)
-      rescue Stripe::APIConnectionError
-        save_error(:connection_problems)
-      rescue Stripe::StripeError
-        save_error(:stripe_error)
-      rescue StandardError
-        save_error(:unknown_error)
-      end
-
-      def charge_and_rescue_data_related_errors
-        create_stripe_charge
-      rescue Stripe::AuthenticationError
-        save_error(:invalid_api_key)
-      rescue Stripe::InvalidRequestError
-        save_error(:invalid_parameter)
-      rescue Stripe::CardError
-        save_error(:declined_card)
-      rescue Stripe::APIError
-        save_error(:invalid_response_object)
-      end
-
-      def create_stripe_charge
-        Stripe.api_key = ENV['STRIPE_SECRET_KEY']
-        Stripe::Charge.create(input_data)
-      end
-
-      def input_data
-        validation.item if validation.okay?
-      end
-
-      def validation
-        @validation ||= InputDataValidator.execute(data)
-      end
-
-      def save_error(error_code)
-        @api_errors << error_code
-        nil
-      end
-
-      def errors
-        validation.errors + api_errors
+      def fields_generator
+        FieldsGenerator.new(data)
       end
     end
   end
--- spec/donation_system/stripe_wrapper/gateway_spec.rb
+++ spec/donation_system/stripe_wrapper/gateway_spec.rb
@@ -9,96 +9,26 @@ module DonationSystem
   module StripeWrapper
     RSpec.describe Gateway do
       let(:data) { VALID_REQUEST_DATA.dup }
+      let(:result) { Result.new({ id: 'id' }, []) }
+
+      before { allow(ResourceCreator).to receive(:create).and_return(result) }
 
       describe 'when successful', vcr: { record: :once } do
-        it 'succeeds with a valid API key and valid parameters' do
-          result = described_class.charge(data)
-          expect(result.item.status).to eq('succeeded')
-          expect(result.errors).to be_empty
-        end
-      end
-
-      describe 'when unsuccessful', vcr: { record: :once } do
-        include Support::WithEnv
-
-        let(:stripe_endpoint) { 'https://api.stripe.com/v1/charges' }
-
-        it 'fails if no data is provided' do
-          result = described_class.charge(nil)
-          expect(result.item).to be_nil
-          expect(result.errors).to include(:missing_data)
-
-          data = RawRequestData.new
-          result = described_class.charge(data)
-          expect(result.item).to be_nil
-          expect(result.errors).to include(:invalid_amount)
+        it 'has no errors' do
+          expect(described_class.charge(data)).to be_okay
+          expect(described_class.charge(data).item).to eq(id: 'id')
         end
 
-        it 'fails without an API key' do
-          with_env('STRIPE_SECRET_KEY' => '') do
-            expect_charge_to_fail_with(:invalid_api_key)
-          end
+        it 'creates a charge' do
+          expect(ResourceCreator).to receive(:create)
+            .with(Stripe::Charge, anything)
+          described_class.charge(data)
         end
 
-        it 'fails with an invalid API key' do
-          with_env('STRIPE_SECRET_KEY' => 'aaaaa') do
-            expect_charge_to_fail_with(:invalid_api_key)
-          end
+        it 'uses the fields for a charge' do
+          expect_any_instance_of(FieldsGenerator).to receive(:for_charge)
+          described_class.charge(data)
         end
-
-        it 'fails with a valid API key but missing parameters' do
-          data = RawRequestData.new(
-            'one-off', '1000', 'brl', '', '', '', '', '', '', '', '', ''
-          )
-          result = described_class.charge(data)
-          expect(result.item).to be_nil
-          expect(result.errors).to include(:invalid_parameter)
-        end
-
-        it 'fails with a valid API key and invalid card number' do
-          data.token = 'tok_chargeDeclined'
-          expect_charge_to_fail_with(:declined_card, data)
-        end
-
-        it 'fails with a valid API key and expired card' do
-          data.token = 'tok_chargeDeclinedExpiredCard'
-          expect_charge_to_fail_with(:declined_card, data)
-        end
-
-        it 'fails due to net conflicts' do
-          stub_request(:post, stripe_endpoint).to_return(status: 409, body: '')
-          expect_charge_to_fail_with(:invalid_response_object)
-        end
-
-        it 'fails due to too many requests' do
-          allow(Stripe::Charge)
-            .to receive(:create)
-            .and_raise(Stripe::RateLimitError)
-          expect_charge_to_fail_with(:too_many_requests)
-        end
-
-        it 'fails due to a Stripe connection error' do
-          allow(Stripe::Charge)
-            .to receive(:create)
-            .and_raise(Stripe::APIConnectionError)
-          expect_charge_to_fail_with(:connection_problems)
-        end
-
-        it 'fails due to Stripe server errors' do
-          allow(Stripe::Charge).to receive(:create).and_raise(Stripe::StripeError)
-          expect_charge_to_fail_with(:stripe_error)
-        end
-
-        it 'fails due to reasons unrelated to Stripe' do
-          allow(Stripe::Charge).to receive(:create).and_raise(StandardError)
-          expect_charge_to_fail_with(:unknown_error)
-        end
-      end
-
-      def expect_charge_to_fail_with(error_code, custom_data = nil)
-        result = described_class.charge(custom_data || data)
-        expect(result.item).to be_nil
-        expect(result.errors).to eq([error_code])
       end
     end
   end
