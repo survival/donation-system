089a4693e15ccb17d9d1354574e9b7c16646a1ac
octopusinvitro <octopusinvitro@users.noreply.github.com>
2018-01-08 16:13:28 +0000

    Update the payment gateway with Stripe one-off and recurring
    At the moment we only have Stripe one-off and recurring donations,
    so for now they are selected here

--- lib/donation_system/payment_gateway.rb
+++ lib/donation_system/payment_gateway.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
+require_relative 'input_data_validator'
+require_relative 'result'
 require_relative 'stripe_wrapper/one_off'
+require_relative 'stripe_wrapper/recurring'
 
 module DonationSystem
   class PaymentGateway
@@ -13,11 +16,25 @@ module DonationSystem
     end
 
     def charge
-      StripeWrapper::OneOff.charge(data)
+      return validation_result unless validation.okay?
+      return StripeWrapper::OneOff.charge(data) if oneoff_donation?
+      StripeWrapper::Recurring.charge(data)
     end
 
     private
 
     attr_reader :data
+
+    def validation
+      @validation ||= InputDataValidator.execute(data)
+    end
+
+    def validation_result
+      Result.new(nil, validation.errors)
+    end
+
+    def oneoff_donation?
+      data.type == InputDataValidator::VALID_TYPES[:oneoff]
+    end
   end
 end
