084ad13a2611c4e805e07d19d507008d92415611
octopusinvitro <octopusinvitro@users.noreply.github.com>
2018-01-08 14:59:31 +0000

    Create a fields generator
    This is the first step in separating the request data validation
    from the Stripe fields generation.
    
    This is done because for recurring donations we have 3 more Stripe
    objects (Plan, Customer and Subscription) that need to be passed
    different fields. However, the validation is the same for all, since
    it deals with the request input data. Hence, this is the right time
    to separate the two tasks (validation and field generation), which
    now change for different reasons.

--- spec/donation_system/data_structs_for_tests.rb
+++ spec/donation_system/data_structs_for_tests.rb
@@ -4,8 +4,21 @@ module DonationSystem
   RawRequestData = Struct.new(:amount, :currency, :giftaid, :token,
                               :name, :email,
                               :address, :city, :state, :zip, :country)
-  StripeCardData = Struct.new(:brand, :last4)
-  RawPaymentData = Struct.new(:id, :status, :amount, :currency, :source, :created)
+  OneOffPaymentData = Struct.new(:id, :status, :amount, :currency, :source,
+                                 :created)
+
+  StripePlanFake = Struct.new(:id)
+
+  StripeCardData = Struct.new(:last4, :brand, :exp_month, :exp_year)
+  VALID_STRIPE_CARD_DATA = StripeCardData.new('4242', 'Visa', '8', '2100')
+
+  StripeCustomerFake = Struct.new(:id) do
+    SourcesFake = Struct.new(:data)
+    def sources
+      SourcesFake.new([VALID_STRIPE_CARD_DATA])
+    end
+  end
+
   SupporterFake = Struct.new(:AccountId)
 
   VALID_REQUEST_DATA = RawRequestData.new(
@@ -13,9 +26,7 @@ module DonationSystem
     'Address', 'City', 'State', 'Z1PC0D3', 'Country'
   ).freeze
 
-  VALID_STRIPE_CARD_DATA = StripeCardData.new('Visa', '4242').freeze
-
-  VALID_PAYMENT_DATA = RawPaymentData.new(
+  VALID_PAYMENT_DATA = OneOffPaymentData.new(
     'ch_1BPDARGjXKYZTzxWrD35FFDc', 'succeeded', 1234, 'gbp',
     VALID_STRIPE_CARD_DATA, 1_510_917_211
   ).freeze
